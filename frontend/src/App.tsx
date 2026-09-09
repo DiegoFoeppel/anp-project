ac540b590c31fc48bf8de91c0accf00883031aa7
Diego Foeppel <diegofoeppel@gmail.com>
2026-09-09 02:23:19 -0300

    base do projeto frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import { MyMap } from "./components/MapExample";
+
+function App() {
+  return <MyMap />;
+}
+
+export default App;
