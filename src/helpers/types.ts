74599efc0c0aa403d6e5f666d594b1640283ec00
Diego Foeppel <diegofoeppel@gmail.com>
2026-09-14 12:03:58 -0300

    alteração arquivo app e helpers

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -27,3 +27,24 @@ export type Preco = {
   produto: string;
   preco: string;
 };
+
+export const colors = {
+  ETANOL: "bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-300",
+  "DIESEL S10":
+    "bg-yellow-50 text-yellow-700 dark:bg-yellow-950 dark:text-yellow-300",
+  "DIESEL S500":
+    "bg-yellow-50 text-yellow-700 dark:bg-yellow-950 dark:text-yellow-300",
+  "GASOLINA COMUM": "bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-300",
+  "GASOLINA ADITIVADA":
+    "bg-blue-50 text-blue-700 dark:bg-blue-950 dark:text-blue-300",
+  GNV: "bg-orange-50 text-orange-700 dark:bg-orange-950 dark:text-orange-300",
+};
+
+export const tiposCombustiveis = [
+  "GASOLINA COMUM",
+  "DIESEL S500",
+  "GNV",
+  "ETANOL",
+  "DIESEL S10",
+  "GASOLINA ADITIVADA",
+];
